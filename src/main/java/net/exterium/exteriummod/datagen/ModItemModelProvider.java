a19d53d3db506314b63afb111cd6ea8134a7634a
aymer31 <aymeric.moncla@gmail.com>
2025-07-29 11:14:59 +0200

    DataGenerators init

---
+++ src/main/java/net/exterium/exteriummod/datagen/ModItemModelProvider.java
@@ -0,0 +1,24 @@
+package net.exterium.exteriummod.datagen;
+
+import net.exterium.exteriummod.ExteriumMod;
+import net.exterium.exteriummod.item.ModItems;
+import net.minecraft.data.PackOutput;
+import net.neoforged.neoforge.client.model.generators.ItemModelProvider;
+import net.neoforged.neoforge.common.data.ExistingFileHelper;
+
+public class ModItemModelProvider extends ItemModelProvider {
+    public ModItemModelProvider(PackOutput output, ExistingFileHelper existingFileHelper) {
+        super(output, ExteriumMod.MOD_ID, existingFileHelper);
+    }
+
+    @Override
+    protected void registerModels() {
+        basicItem(ModItems.BISMUTH.get());
+        basicItem(ModItems.RAW_BISMUTH.get());
+
+        basicItem(ModItems.RADISH.get());
+        basicItem(ModItems.STARLIGHT_ASHES.get());
+        basicItem(ModItems.FROSTFIRE_ICE.get());
+        basicItem(ModItems.CHISEL.get());
+    }
+}
